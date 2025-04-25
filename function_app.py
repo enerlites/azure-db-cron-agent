fbce0f5548f0b4cd67a170c00f7f4be176d3144a
Andrew Chen <andrew.chen@enerlites.com>
2025-04-25 16:01:27 -0700

    update invocation log

--- function_app.py
+++ function_app.py
@@ -6,12 +6,13 @@ app = func.FunctionApp()
 
 '''
 auto oneDrive azure read / write in monthly basis 
+Execute cron trigger on 12:30 AM UTC on 15th of each month
 '''
 @app.function_name(name="monthlySkuPromoTask")
 @app.schedule(schedule="0 30 0 15 * *", arg_name="monthlyPromoCron", run_on_startup=True, use_monitor=True)
 def monthly_sku_promo_task(monthlyPromoCron: func.TimerRequest) -> None:
     # Log that the function is starting
-    logging.info(f'========================= Monthly SKU Promo Job Started ! =========================\n')
+    logging.info(f'========================= Invoke monthly_promotion_brochure_job() ! =========================\n')
     
     try:
         # call func from module
@@ -19,17 +20,18 @@ def monthly_sku_promo_task(monthlyPromoCron: func.TimerRequest) -> None:
         
     except Exception as e:
         # Log any errors
-        logging.error(f'>>>>>>>>>>>>>>>>>>>> Error in Monthly SKU Promo Job <<<<<<<<<<<<<<<<<<<<<\n {str(e)}')
+        logging.error(f'>>>>>>>>>>>>>>>>>>>> Error in monthly_promotion_brochure_job() <<<<<<<<<<<<<<<<<<<<<\n {str(e)}')
         raise
     
 '''
 Automate potential ERP reports to Azure
+Execute cron trigger on 1:00 AM UTC on 15th of each month
 '''
 @app.function_name(name="monthlyERPTask")
 @app.schedule(schedule="0 0 1 15 * *", arg_name="monthlyERPCron", run_on_startup=True, use_monitor=True)
 def monthly_erp_task(monthlyERPCron: func.TimerRequest) -> None:
     # Log that the function is starting
-    logging.info(f'========================= Monthly SKU Promo Job Started ! =========================\n')
+    logging.info(f'========================= Invoke monthly_netsuite_erp_job() ! =========================\n')
     
     try:
         # call func from module
@@ -37,5 +39,5 @@ def monthly_erp_task(monthlyERPCron: func.TimerRequest) -> None:
         
     except Exception as e:
         # Log any errors
-        logging.error(f'>>>>>>>>>>>>>>>>>>>> Error in Monthly SKU Promo Job <<<<<<<<<<<<<<<<<<<<<\n {str(e)}')
+        logging.error(f'>>>>>>>>>>>>>>>>>>>> Error in monthly_netsuite_erp_job() <<<<<<<<<<<<<<<<<<<<<\n {str(e)}')
         raise
