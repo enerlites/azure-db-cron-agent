890f9307de647bfe878b9673b5aaed7fee6621cd
Andrew Chen <andrew.chen@enerlites.com>
2025-04-30 11:08:14 -0700

    update respective cron job execution time

--- function_app.py
+++ function_app.py
@@ -6,38 +6,47 @@ app = func.FunctionApp()
 
 '''
 auto oneDrive azure read / write in monthly basis 
-Execute cron trigger on 12:30 AM UTC on 15th of each month
+Execute cron trigger on 12:30 AM PST on 15th of each month
 '''
 @app.function_name(name="monthlySkuPromoTask")
-@app.schedule(schedule="0 30 0 15 * *", arg_name="monthlyPromoCron", run_on_startup=True, use_monitor=True)
+@app.schedule(schedule="0 30 8 15 * *", arg_name="monthlyPromoCron", run_on_startup=True, use_monitor=True)
 def monthly_sku_promo_task(monthlyPromoCron: func.TimerRequest) -> None:
-    # Log that the function is starting
     logging.info(f'========================= Invoke monthly_promotion_brochure_job() ! =========================\n')
     
     try:
-        # call func from module
         monthly_promotion_brochure_job()
         
     except Exception as e:
-        # Log any errors
         logging.error(f'>>>>>>>>>>>>>>>>>>>> Error in monthly_promotion_brochure_job() <<<<<<<<<<<<<<<<<<<<<\n {str(e)}')
         raise
     
 '''
 Automate potential ERP reports to Azure
-Execute cron trigger on 1:00 AM UTC on 15th of each month
+Execute cron trigger on 1:00 AM PST on 15th of each month
 '''
 @app.function_name(name="monthlyERPTask")
-@app.schedule(schedule="0 0 1 15 * *", arg_name="monthlyERPCron", run_on_startup=True, use_monitor=True)
+@app.schedule(schedule="0 0 9 15 * *", arg_name="monthlyERPCron", run_on_startup=True, use_monitor=True)
 def monthly_erp_task(monthlyERPCron: func.TimerRequest) -> None:
-    # Log that the function is starting
     logging.info(f'========================= Invoke monthly_netsuite_erp_job() ! =========================\n')
     
     try:
-        # call func from module
         monthly_netsuite_erp_job()
         
     except Exception as e:
-        # Log any errors
         logging.error(f'>>>>>>>>>>>>>>>>>>>> Error in monthly_netsuite_erp_job() <<<<<<<<<<<<<<<<<<<<<\n {str(e)}')
+        raise
+
+'''
+Automate competitor pricing to Azure
+Execute cron trigger on 11:30 PM PST on every day
+'''
+@app.function_name(name="dailyCompPricingTask")
+@app.schedule(schedule="30 7 * * *", arg_name="dailyCompPricingCron", run_on_startup=True, use_monitor=True)
+def daily_comp_pricing_task(dailyCompPricingCron: func.TimerRequest) -> None:
+    logging.info(f'========================= Invoke daily daily_comp_pricing_job() ! =========================\n')
+    
+    try:
+        daily_comp_pricing_job()
+    except Exception as e:
+        logging.error(f'>>>>>>>>>>>>>>>>>>>> Error in daily_comp_pricing_job() <<<<<<<<<<<<<<<<<<<<<\n {str(e)}')
         raise
