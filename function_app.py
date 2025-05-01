f6e79351984812bdd4d1375dd93c027423d82734
Andrew Chen <andrew.chen@enerlites.com>
2025-05-01 11:56:28 -0700

    add comments

--- function_app.py
+++ function_app.py
@@ -4,18 +4,21 @@ from azureApp import *
 
 app = func.FunctionApp()
 
+# Run on 12:30 AM PDT on 15th of each month
 @app.function_name(name="monthlySkuPromoTask")
 @app.schedule(schedule="0 30 8 15 * *", arg_name="monthlyPromoCron", run_on_startup=True, use_monitor=True)
 def monthly_sku_promo_task(monthlyPromoCron: func.TimerRequest) -> None:
     logging.info("Running: monthly_promotion_brochure_job()")
     monthly_promotion_brochure_job()
 
+# Run on 1:00 AM PDT on 15th of each month
 @app.function_name(name="monthlyERPTask")
 @app.schedule(schedule="0 0 9 15 * *", arg_name="monthlyERPCron", run_on_startup=True, use_monitor=True)
 def monthly_erp_task(monthlyERPCron: func.TimerRequest) -> None:
     logging.info("Running: monthly_netsuite_erp_job()")
     monthly_netsuite_erp_job()
 
+# Run on 23:00 PM PDT every day
 @app.function_name(name="dailyCompPricingTask")
 @app.schedule(schedule="30 7 * * *", arg_name="dailyCompPricingCron", run_on_startup=True, use_monitor=True)
 def daily_comp_pricing_task(dailyCompPricingCron: func.TimerRequest) -> None:
